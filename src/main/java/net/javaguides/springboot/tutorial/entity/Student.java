6dab2cadac1563d9c8e254b5ca50946ac3af59d6
longnv1511 <longnv1511@gmail.com>
2020-10-07 11:40:58 +0700

    Init source

---
+++ src/main/java/net/javaguides/springboot/tutorial/entity/Student.java
@@ -0,0 +1,64 @@
+package net.javaguides.springboot.tutorial.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "student")
+public class Student {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @Column(name = "name")
+    private String name;
+    
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_no")
+    private long phoneNo;
+    
+    public Student() {}
+
+    public Student(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    public long getId() {
+        return id;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+}
